394b3a3709579097a18292a10d776c6743027c6a
dusmart <dusmart@qq.com>
2022-02-15 16:23:06 +0800

    fix redundant import

--- dao/vote.go
+++ dao/vote.go
@@ -2,12 +2,10 @@ package dao
 
 import (
 	"math/big"
-
-	"github.com/nspcc-dev/neo-go/pkg/util"
 )
 
 type Vote struct {
-	Proposer Voter
+	Proposer   Voter
 	ProposalID big.Int
-	accept bool
+	accept     bool
 }
