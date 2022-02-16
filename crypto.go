3e3556f23e5efe1edad928ebfc11fa1d8bd34888
dusmart <dusmart@qq.com>
2022-02-16 15:35:28 +0800

    add check sig

--- crypto.go
+++ crypto.go
@@ -1,38 +1,8 @@
 package main
 
 import (
-	"encoding/hex"
-
-	"github.com/nspcc-dev/neo-go/pkg/crypto/hash"
-	"github.com/nspcc-dev/neo-go/pkg/crypto/keys"
 )
 
 func init() {
-	pubKey, _ := keys.NewPublicKeyFromString("021319f4f1ece7181760b876dcc08f91dbffc4b55743c78a9fe827a2cc7a8e2600")
-	salt := "1e42534301a51fd977f919850f1dcbc7"
-	message := hex.EncodeToString([]byte(salt + "[860833102,0,true]"))
-	message = hex.EncodeToString([]byte{byte(len(message) / 2)}) + message
-	message = "010001f0" + message + "0000"
-	msg, _ := hex.DecodeString(message)
-	hashedData := hash.Sha256(msg)
-	signature := "00cdb322937e6dbe233c019a9583fd27f4cba2b6055291279eaaeb70fc93d28f1621de6d01a17c2a894232d7b7d0cf54382018e2e2630f0f171f495d8200e1f4"
-	sig, _ := hex.DecodeString(signature)
-	println(pubKey.Verify(sig, hashedData.BytesBE()))
-}
 
-/*
-export function num2VarInt(num: number): string {
-  if (num < 0xfd) {
-    return num2hexstring(num);
-  } else if (num <= 0xffff) {
-    // uint16
-    return "fd" + num2hexstring(num, 2, true);
-  } else if (num <= 0xffffffff) {
-    // uint32
-    return "fe" + num2hexstring(num, 4, true);
-  } else {
-    // uint64
-    return "ff" + num2hexstring(num, 8, true);
-  }
 }
-*/
--- dao/vote.go
+++ dao/vote.go
@@ -1,8 +1,12 @@
 package dao
 
 import (
+	"encoding/hex"
 	"fmt"
 	"math/big"
+
+	"github.com/nspcc-dev/neo-go/pkg/crypto/hash"
+	"github.com/nspcc-dev/neo-go/pkg/crypto/keys"
 )
 
 type Vote struct {
@@ -15,4 +19,20 @@ type Vote struct {
 
 func (v *Vote) Message() string {
 	return fmt.Sprintf("[%d,%d,%t]", 860833102, v.ProposalID, v.Accept)
+}
+
+func (v *Vote) CheckSig() bool {
+	pubKey := keys.PublicKey(v.Voter)
+	message := hex.EncodeToString([]byte(v.Salt + v.Message()))
+	message = fmt.Sprintf("%s%s%s%s", "010001f0", hex.EncodeToString([]byte{byte(len(message) / 2)}), message, "0000")
+	msg, err := hex.DecodeString(message)
+	if err != nil {
+		panic(err)
+	}
+	hashedData := hash.Sha256(msg)
+	sig, err := hex.DecodeString(v.Signature)
+	if err != nil {
+		return false
+	}
+	return pubKey.Verify(sig, hashedData.BytesBE())
 }
