2beafd2c37ab7346cb65352ac317d7b62071dc6e
vang1ong7ang <i@7.day>
2022-02-16 12:21:00 +0000

    dev

--- config.go
+++ config.go
@@ -1,19 +1,32 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
 var config struct {
-	github_token string
-	github_repo  string
+	github_token   string
+	github_owner   string
+	github_repo    string
 	listen_address string
 }
+var client *github.Client
 
 func init() {
 	config.github_token = os.ExpandEnv("${GITHUBTOKEN}")
+	config.github_owner = os.ExpandEnv("${GITHUBOWNER}")
 	config.github_repo = os.ExpandEnv("${GITHUBREPO}")
 	config.listen_address = os.ExpandEnv(":${PORT}")
 	log.Println("[CONFIG]: ", config)
+
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: config.github_token},
+	)
+	tc := oauth2.NewClient(context.Background(), ts)
+	client = github.NewClient(tc)
 }
