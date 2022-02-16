f1c2ec2c19cd4938c566a45737df681f600bc14f
vang1ong7ang <i@7.day>
2022-02-16 04:54:16 +0000

    dev

--- http.go
+++ http.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 func init() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("ok"))
+		result := struct {
+			Name      string
+			Timestamp int64
+		}{
+			os.Args[0],
+			time.Now().Unix(),
+		}
+		if err := json.NewEncoder(w).Encode(result); err != nil {
+			log.Println("[ERROR]: ", err)
+		}
 	})
 }
