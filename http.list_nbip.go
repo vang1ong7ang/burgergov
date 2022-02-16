86246c536f6049d8530a72f8f74b184bab695c14
Hecate2 <2474101468@qq.com>
2022-02-16 14:49:00 +0800

    log.Fatal->log.Print; use github token

--- http.list_nbip.go
+++ http.list_nbip.go
@@ -11,9 +11,12 @@ import (
 func init() {
 	http.HandleFunc("/list_nbip", func(w http.ResponseWriter, r *http.Request) {
 		// get all branch names starts with `NBIP-`
-		resp, err := http.Get("https://api.github.com/repos/" + config.github_repo + "/branches")
+		client := http.Client{}
+		req, _ := http.NewRequest("GET", "https://api.github.com/repos/" + config.github_repo + "/branches", nil)
+		req.Header.Add("Authorization", "token "+config.github_token)
+		resp, err := client.Do(req)
 		if err != nil {
-			log.Fatal(err)
+			log.Print(err)
 		}
 		defer resp.Body.Close()
 		body, _ := ioutil.ReadAll(resp.Body)
