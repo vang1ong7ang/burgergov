787a99e583c730d3c7af0bfd630e66a93d5434cb
Hecate2 <2474101468@qq.com>
2022-02-16 13:50:37 +0800

    return list of branch names

--- http.list_nbip.go
+++ http.list_nbip.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -9,7 +10,18 @@ import (
 func init() {
 	http.HandleFunc("/list_nbip", func(w http.ResponseWriter, r *http.Request) {
 		// get all branch names starts with `NBIP-`
+		resp, err := http.Get("https://api.github.com/repos/" + config.github_repo + "/branches")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer resp.Body.Close()
+		body, _ := ioutil.ReadAll(resp.Body)
+		var branches []struct{ Name string }
+		json.Unmarshal(body, &branches)
 		result := []string{}
+		for _, branch := range branches {
+			result = append(result, branch.Name)
+		}
 		if err := json.NewEncoder(w).Encode(result); err != nil {
 			log.Println("[ERROR]: ", err)
 		}
