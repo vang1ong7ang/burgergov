d39a028c28087b5348394b7539d3779098ac6834
vang1ong7ang <i@7.day>
2022-02-16 05:17:25 +0000

    dev

--- http.submit_vote.go
+++ http.submit_vote.go
@@ -1,20 +1,10 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
 	"net/http"
 )
 
 func init() {
-	http.HandleFunc("/submit_vote", func(w http.ResponseWriter, r *http.Request) {
-		// get the `readme.md` and `nbip.json`
-		result := struct {
-			README string
-			NBIP   interface{}
-		}{}
-		if err := json.NewEncoder(w).Encode(result); err != nil {
-			log.Println("[ERROR]: ", err)
-		}
+	http.HandleFunc("/vote", func(w http.ResponseWriter, r *http.Request) {
 	})
 }
