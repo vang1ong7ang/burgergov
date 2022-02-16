e5d1eded94b247cb5b0fad2e0debe8412039741b
vang1ong7ang <i@7.day>
2022-02-16 05:53:17 +0000

    dev

--- http.nbip.go
+++ http.nbip.go
@@ -4,15 +4,36 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func init() {
 	http.HandleFunc("/nbip/", func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		dir := filepath.Dir(path)
+		target := filepath.Base(path)
+		head := filepath.Dir(dir)
+		id := filepath.Base(dir)
+		if head != "/nbip" {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		println(id)
+		switch target {
+		case "README.md":
+			// todo
+		case "nbip.json":
+			// todo
+		case "all.json":
+			// todo
+		}
+
+		// to be removed
+		
 		// /nbip/{NUM}/nbip.json
 		// /nbip/{NUM}/README.md
 		// /nbip/{NUM}/all.json
 		
-		// log.Println(r.URL.Path)
 		
 		// all.json
 		result := struct {
