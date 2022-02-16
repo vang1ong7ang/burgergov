73c6576de28c4781cdf1956955410a21928ede03
vang1ong7ang <i@7.day>
2022-02-16 04:47:59 +0000

    dev

---
+++ http.get_nbip.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/get_nbip", func(w http.ResponseWriter, r *http.Request) {
+		// get the `readme.md` and `nbip.json`
+		result := struct{ README string; NBIP interface{}}{}
+		if err := json.NewEncoder(w).Encode(result); err != nil {
+			log.Println("[ERROR]: ", err)
+		}
+	})
+}
