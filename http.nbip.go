bac5cbff9f8fc67492a9f0ca9148b4dea469d478
Hecate2 <2474101468@qq.com>
2022-02-16 15:27:29 +0800

    service to get README.md, nbip.json, all.json

--- http.nbip.go
+++ http.nbip.go
@@ -2,10 +2,33 @@ package main
 
 import (
 	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"path/filepath"
 )
+
+func get_text_content(w http.ResponseWriter, r *http.Request, target string, id string) []byte {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://api.github.com/repos/" + config.github_repo + "/contents/" + target + "?ref=NBIP-" + id, nil)
+	req.Header.Add("Authorization", "token "+config.github_token)
+	resp, err := client.Do(req)
+	if err != nil {log.Println(err)}
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+	var downloadUrl struct{ Download_url string }
+	json.Unmarshal(body, &downloadUrl)
+	if downloadUrl.Download_url == ""{
+		http.NotFound(w, r)
+		return nil
+	}
+
+	resp, err = http.Get(downloadUrl.Download_url)
+	if err != nil {log.Println(err)}
+	defer resp.Body.Close()
+	result, _ := io.ReadAll(resp.Body)
+	return result
+}
 
 func init() {
 	http.HandleFunc("/nbip/", func(w http.ResponseWriter, r *http.Request) {
@@ -14,35 +37,34 @@ func init() {
 		target := filepath.Base(path)
 		head := filepath.Dir(dir)
 		id := filepath.Base(dir)
-		if head != "/nbip" {
-			w.WriteHeader(http.StatusNotFound)
+		if head != "/nbip" && head != "\\nbip" {
+			http.NotFound(w, r)
 			return
 		}
-		println(id)
 		switch target {
 		case "README.md":
-			// todo
+			fallthrough
 		case "nbip.json":
-			// todo
+			result := get_text_content(w, r, target, id)
+			w.Write(result)
 		case "all.json":
-			// todo
-		}
-
-		// to be removed
-		
-		// /nbip/{NUM}/nbip.json
-		// /nbip/{NUM}/README.md
-		// /nbip/{NUM}/all.json
-		
-		
-		// all.json
-		result := struct {
-			README string
-			NBIP   interface{}
-		}{}
-		
-		if err := json.NewEncoder(w).Encode(result); err != nil {
-			log.Println("[ERROR]: ", err)
+			//result := struct {
+			//	README string
+			//	NBIP   string
+			//}{string(get_text_content(w, r, "README.md", id)), string(get_text_content(w, r, "nbip.json", id))}
+			//if err := json.NewEncoder(w).Encode(result); err != nil {
+			//	log.Println("[ERROR]: ", err)
+			//}
+			result := struct {
+				README []byte
+				NBIP   []byte
+			}{get_text_content(w, r, "README.md", id), get_text_content(w, r, "nbip.json", id)}
+			if err := json.NewEncoder(w).Encode(result); err != nil {
+				log.Println("[ERROR]: ", err)
+			}
+		default:
+			http.NotFound(w, r)
+			return
 		}
 	})
 }
